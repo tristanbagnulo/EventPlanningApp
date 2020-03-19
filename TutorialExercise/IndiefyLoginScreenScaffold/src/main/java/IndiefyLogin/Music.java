621e09d3ef2adc3e2f45b6429a5c8a633cebfd6d
z5191687 <61257076+z5191687@users.noreply.github.com>
2020-03-19 12:28:14 +1100

    Update Music.java

--- TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
+++ TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
@@ -1,14 +1,19 @@
 public class Music {
     
-    private StringProperty album;
+    private String album;
     private String artist;
     private String genre;
     private String year;
    // Add the other attributes for the Music List
     
     public Music(String album, String artist, String genre, String year) {
+<<<<<<< HEAD
         this.album = album; 
         this.artist = artist; 
+=======
+        this.album = album;
+        this.artist = artist;
+>>>>>>> b10f87031e8c55f1edcd82f52147d85a4b7e4e9d
         this.genre = genre;
         this.year = year;
     }
@@ -17,7 +22,7 @@ public class Music {
         this.album = new SimpleStringProperty(album);
         // Complete the constructor
     }
-    
+    //What's good.
    // Add getters for String Properties
     public StringPropery getAlbum(){
         return album;
