1af0c85bef78b47e84bddee9e6301d5cfc960076
beezwak <61256469+beezwak@users.noreply.github.com>
2020-03-19 12:51:46 +1100

    deleted hash stuff
    delete

--- TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
+++ TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
@@ -7,13 +7,10 @@ public class Music {
    // Add the other attributes for the Music List
     
     public Music(String album, String artist, String genre, String year) {
-<<<<<<< HEAD
         this.album = album; 
         this.artist = artist; 
-=======
         this.album = album;
         this.artist = artist;
->>>>>>> b10f87031e8c55f1edcd82f52147d85a4b7e4e9d
         this.genre = genre;
         this.year = year;
     }
