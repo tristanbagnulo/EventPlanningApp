27a65f849e6a6ac32188e085a082bfe218e5f3e5
beezwak <61256469+beezwak@users.noreply.github.com>
2020-03-19 12:10:03 +1100

    created music constructor
    created music constructor with album, artist, genre and year

--- TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
+++ TutorialExercise/IndiefyLoginScreenScaffold/src/main/java/IndiefyLogin/Music.java
@@ -1,13 +1,16 @@
 public class Music {
     
-    private StringProperty album;
+    private String album;
     private String artist;
     private String genre;
     private String year;
    // Add the other attributes for the Music List
     
-    public Music() {
-        this("","","","");
+    public Music(String album, String artist, String genre, String year) {
+        this.album = album;
+        this.artist = artist;
+        this.genre = genre;
+        this.year = year;
     }
 
     public Music(String album) {
