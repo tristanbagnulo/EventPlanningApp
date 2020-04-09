4bdad32303d5e4391f7ae244b5f1eed6bbabb434
beezwak <61256469+beezwak@users.noreply.github.com>
2020-04-09 13:49:20 +1000

    update commit
    updated commit from last

---
+++ GroupAssignment/src/main/java/venuebrain/adminDashboardController.java
@@ -0,0 +1,30 @@
+package venuebrain;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+//comment to help commit work
+//another
+
+public class adminDashboardController {
+     @FXML
+    // Initialise the TableView as FXML variables
+    TableView dashboard = new TableView();
+   
+    // Initialise the TableColumns as FXML variables
+//    @FXML
+//   TableColumn<event, Integer> existingColumn = new TableColumn<>("Existing Events");
+//    
+//    @FXML
+//   TableColumn<> createdByColumn = new TableColumn<>("Created by");
+    
+
+// @FXML
+//    private void existingEventsColumn() throws IOException, SQLException {
+//         
+//    }
+}
