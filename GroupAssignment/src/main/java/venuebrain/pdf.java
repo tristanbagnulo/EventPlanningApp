d3370d5d262b5606b09ee1923d4a2f22ee05edd7
beezwak <61256469+beezwak@users.noreply.github.com>
2020-04-18 17:55:17 +1000

    created the rsvp class
    created the rsvp class and worked more on the pdf class

--- GroupAssignment/src/main/java/venuebrain/pdf.java
+++ GroupAssignment/src/main/java/venuebrain/pdf.java
@@ -8,25 +8,24 @@ import org.w3c.dom.Document;
  * @author Shai C
  */
 public class pdf {
+   
     
-//    public static void main(String[] args){
-//    
-//       try{
-//       Document document = new Document();
-//       PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
-//
-//       
-//       document.add(new Paragraph("Please Print please please test test"));
-//       
-//       document.open();
-//       Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
-//       Chunk chunk = new Chunk("Hello World", font);
-//
-//       document.add(chunk);
-//       document.close();
-//       }catch(Exception e){
-//           System.out.println(e);
-//       }
-//       System.out.println("itext executed");
-//    }
+       try{
+       Document document = new Document();
+       PdfWriter.getInstance(document, new FileOutputStream("iTextHelloWorld.pdf"));
+
+       
+       document.add(new Paragraph("Please Print please please test test"));
+       
+       document.open();
+       Font font = FontFactory.getFont(FontFactory.COURIER, 16, BaseColor.BLACK);
+       Chunk chunk = new Chunk("Hello World", font);
+
+       document.add(chunk);
+       document.close();
+       }catch(Exception e){
+           System.out.println(e);
+       }
+       System.out.println("itext executed");
+    
 }
